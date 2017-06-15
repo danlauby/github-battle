06153f5c8bad19394ce173d53c32fce18263c5e0
danlauby <dmlauby@gmail.com>
2017-06-15 15:20:49 -0700

    Make api request in utils/api.js and render state in Popular Component based on language selection

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import api from '../utils/api';
 
 
 function SelectLanguage (props) {
@@ -30,18 +31,32 @@ class Popular extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  componentDidMount () {
+    this.updateLanguage(this.state.selectedLanguage);
   }
 
   updateLanguage(lang) {
     this.setState(function () {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
-    })
+    });
+
+    api.fetchPopularRepos(lang)
+    .then(function (repos) {
+      this.setState(function () {
+        return {
+          repos: repos,
+        }
+      })
+    }.bind(this));
   }
   render() {
     return (
@@ -50,6 +65,7 @@ class Popular extends React.Component {
         selectedLanguage={this.state.selectedLanguage}
         onSelect={this.updateLanguage}
         />
+        {JSON.stringify(this.state.repos, null, 2)}
       </div>
     )
   }
