8a2f5938b9694b8536e88936cf4e4b5bee811777
danlauby <dmlauby@gmail.com>
2017-06-15 14:44:21 -0700

    Add Select Component that builds Popular nav with event listeners and styling

---
+++ app/components/Popular.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+class Popular extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+        selectedLanguage={this.state.selectedLanguage}
+        onSelect={this.updateLanguage}
+        />
+      </div>
+    )
+  }
+}
+
+module.exports = Popular;
