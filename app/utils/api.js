0f7df9de35f4df1d9003722bbd40975bc5302b00
danlauby <dmlauby@gmail.com>
2017-06-21 10:42:19 -0700

    Refactor api.js to ES6

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,59 +2,57 @@
 import axios from 'axios';
 
 {/* Add key and secret if api requires it */}
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec;
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = "?client_id=" + id + "&client_secret=" + sec;
 
 {/* Request username profiles object from Github */}
 {/*  Return users with Axios library */}
 {/* Promise gets user data after request is complete */}
-function getProfile (username) {
-  return axios.get('https://api.github.com/users/' + username + params)
-    .then(function (user) {
+const getProfile = (username) => {
+   axios.get('https://api.github.com/users/' + username + params)
+    .then((user) => {
       return user.data;
     });
 }
 
 {/* Request repos object from Github */}
 {/* Return repos with Axios library, limit  to 100 repos per page */}
-function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+const getRepos = (username) => {
+  axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
 }
 
 {/* Collect stargazer stars from repos and reduce array to a single number  */}
-function getStarCount (repos) {
-  return repos.data.reduce(function (count, repo) {
-    return count + repo.stargazers_count;
+const getStarCount = (repos) => {
+  repos.data.reduce((count, repo) => {
+    count + repo.stargazers_count;
   }, 0);
 }
 
 {/* Store followers property from profile object */}
-{/* Call getStarCount method on repos object and store in followers variable */}
+{/* Call getStarCount method on repos object and store in followers constiable */}
 {/* Multiply users followers by 3, then add stars to detemine winner/loser */}
-function calculateScore (profile, repos) {
-  var followers = profile.followers;
-  var totalStars = getStarCount(repos);
-  return (followers * 3) + totalStars;
+const calculateScore = (profile, repos) => {
+  const followers = profile.followers;
+  const totalStars = getStarCount(repos);
+    (followers * 3) + totalStars;
 }
-
 
 {/* Log error to console if !null */}
-function handleError (error) {
+const handleError = (error) => {
   console.warn(error);
-  return null;
+    null;
 }
-
 
 {/* Call api requests and get all requests in promise and return in order */}
 {/* Store profile and calculated score as properties */}
-function getUserData (player) {
-  return axios.all([
+const getUserData = (player) => {
+  axios.all([
     getProfile(player),
     getRepos(player)
-  ]).then(function (data) {
-    var profile = data[0];
-    var repos = data[1];
+  ]).then((data) => {
+    const profile = data[0];
+    const repos = data[1];
     return {
       profile: profile,
       score: calculateScore(profile, repos)
@@ -63,10 +61,10 @@ function getUserData (player) {
 }
 
 {/* Sort players by subtracting loser score from winners score */}
-function sortPlayers (players) {
-    return players.sort(function (a,b) {
-      return b.score - a.score;
-    })
+const sortPlayers = (players) => {
+  players.sort((a,b) => {
+    b.score - a.score;
+  })
 }
 
 {/* Export promise with profile, score of each player */}
@@ -75,15 +73,15 @@ function sortPlayers (players) {
 {/* Return popular repos (limit: 100) after encoding */}
 {/* Export api to Results.js, Popular.js */}
 module.exports = {
-  battle: function (players) {
-    return axios.all(players.map(getUserData))
+  battle: (players) => {
+    axios.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError)
   },
-  fetchPopularRepos: function (language) {
-    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+  fetchPopularRepos: (language) => {
+    const encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
     return axios.get(encodedURI)
-      .then(function (response) {
+      .then((response) => {
         return response.data.items;
       });
   }
